be914efc4e8f039eb76864859dbcd467c1184e4d
KavyaPai6 <kavya.pai@juegostudioz.com>
2023-10-31 12:27:07 +0530

    FEAT: Updated side navigation bar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { FaAngleLeft, FaAngleRight, FaSignOutAlt, FaTable } from "react-icons/fa";
+import {
+  FaAngleLeft,
+  FaAngleRight,
+  FaSignOutAlt,
+  FaMapMarkedAlt
+} from "react-icons/fa";
 import { TbBrandGoogleBigQuery } from "react-icons/tb";
 import styled from "styled-components";
 import AppColors from "../styles/colors";
@@ -13,15 +18,15 @@ interface LayoutProps {
 
 const NavButtonData = [
   {
-    label: "Vessel Query",
+    label: "Position Map",
     link: AppRoutes.VesselQuery,
-    icon: <TbBrandGoogleBigQuery size={25} />,
+    icon: <FaMapMarkedAlt size ={25}/>,
     isPublic: true,
   },
   {
     label: "Vessel ETA",
     link: AppRoutes.VesselETA,
-    icon: <FaTable size={25} />,
+    icon: <TbBrandGoogleBigQuery size={25} />,
     isPublic: true,
   },
   {
@@ -34,28 +39,28 @@ const NavButtonData = [
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [showMenu, setShowMenu] = useState(true);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const location = useLocation();
   return (
     <DrawerContainer>
       <SideMenu $showMenu={showMenu}>
         <LogoContainer>
-            <Image src="https://sgtradex.com/images/sgtradex-logo.svg"/>
+          <Image src="https://sgtradex.com/images/sgtradex-logo.svg" />
         </LogoContainer>
         <NavItem>
           {NavButtonData.map(
             (btnData, index) =>
               btnData.isPublic && (
-                    <NavLink
-                    $isActive={location.pathname == btnData.link}
-                    $showMenu={showMenu}
-                    onClick={() => navigate(btnData.link)}
-                    key={index}
-                  >
-                    <NavIcon>{btnData.icon}</NavIcon>
-                    {showMenu && <NavTitle>{btnData.label}</NavTitle>}
-                  </NavLink>
-                                )
+                <NavLink
+                  $isActive={location.pathname == btnData.link}
+                  $showMenu={showMenu}
+                  onClick={() => navigate(btnData.link)}
+                  key={index}
+                >
+                  <NavIcon>{btnData.icon}</NavIcon>
+                  {showMenu && <NavTitle>{btnData.label}</NavTitle>}
+                </NavLink>
+              )
           )}
         </NavItem>
       </SideMenu>
@@ -67,7 +72,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             setShowMenu(!showMenu);
           }}
         >
-          {showMenu ?  <FaAngleRight size={24}/> : <FaAngleLeft size={24}/> }
+          {showMenu ? <FaAngleLeft size={24} /> : <FaAngleRight size={24} />}
         </DrawerIconContainer>
         <PageContent>{children}</PageContent>
       </Page>
@@ -83,65 +88,66 @@ const DrawerContainer = styled.div`
   height: 100vh;
 `;
 const NavLink = styled.a<{ $showMenu: boolean; $isActive: boolean }>`
-    display: flex;
-    align-items: center;
-    color: ${AppColors.White};
-    font-weight: 600;
-    width: 100%;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    padding: 0.8rem 0;
-    justify-content: ${(props) => (props.$showMenu ? `flex-start` : `center`)};
-    background: ${(props) => (props.$isActive ? AppColors.ThemeSkyBlue : "")};
-    text-decoration: none;
-    border-bottom: 1px solid ${AppColors.ThemeTransparencyWhite};
-    &:hover{
-      background: ${AppColors.ThemeSkyBlue};
-    }
+  display: flex;
+  align-items: center;
+  color: ${AppColors.White};
+  font-weight: 600;
+  width: 100%;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding: 0.8rem 0;
+  background: ${(props) => (props.$isActive ? AppColors.ThemeSkyBlue : "")};
+  text-decoration: none;
+  border-bottom: 1px solid ${AppColors.ThemeTransparencyWhite};
+  &:hover {
+    background: ${AppColors.ThemeSkyBlue};
+  }
 `;
 
 const DrawerIconContainer = styled(NavLink)`
   cursor: pointer;
   width: max-content;
-  background:  ${AppColors.ThemeLightBlack};
-  border-radius:0.2rem;
-  padding:0.5rem;
+  background: ${AppColors.ThemeLightBlack};
+  border-radius: 0.2rem;
+  padding: 0.5rem;
   margin: 1rem;
   position: absolute;
-  &:hover{
-    background:  ${AppColors.ThemeLightBlack}
+  z-index: 10;
+  &:hover {
+    background: ${AppColors.ThemeLightBlack};
   }
 `;
 
 const LogoContainer = styled.div`
-    ${sharedFlexCenter}
-    height: 12%;
-    padding-top: 5.5rem;
-`
+  ${sharedFlexCenter}
+  height: 12%;
+  padding-top: 5.5rem;
+`;
 
 const SideMenu = styled.div<{ $showMenu: boolean }>`
   height: 100vh;
   ${sharedFlexCenter}
   flex-direction: column;
   background-color: ${AppColors.ThemeLightBlack};
-  width: ${(props) => (props.$showMenu ? `20rem` : `0%`)}
+  transition: 0.5s;
+  width: ${(props) => (props.$showMenu ? `20rem` : `0%`)};
 `;
 
 const NavItem = styled.div`
   height: 90%;
   width: 100%;
-  display:flex;
-  align-Items: center;
+  display: flex;
+  align-items: center;
   flex-direction: column;
   padding-top: 2.5rem;
   margin-left: 0.5rem;
   margin-right: 0.5rem;
-  
+  color: ${AppColors.White}
 `;
 
 const NavIcon = styled.div`
   padding-left: 1rem;
-`
+`;
 
 const NavTitle = styled.div`
   margin-left: 1.25rem;
@@ -157,4 +163,3 @@ const PageContent = styled.div`
   ${sharedFlexCenter}
   width: 100%;
 `;
-
